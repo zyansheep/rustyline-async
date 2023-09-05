65708c5e00e9b3174ecfb2c0ee739f5046c6b85c
zyansheep <zyansheep@protonmail.com>
2023-09-05 10:37:27 -0400

    test previous fix

--- examples/test-readline.rs
+++ examples/test-readline.rs
@@ -6,6 +6,9 @@ use tokio::time::sleep;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (mut rl, mut stdout) = Readline::new("prompt> ".into())?;
+
+    rl.should_print_line_on(false, false);
+
     loop {
         tokio::select! {
             _ = sleep(Duration::from_secs(1)) => {
@@ -23,7 +26,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     writeln!(stdout, "<EOF>")?;
                     break;
                 }
-                Ok(ReadlineEvent::Interrupted) => {writeln!(stdout, "^C")?; continue; }
+                Ok(ReadlineEvent::Interrupted) => {
+                    // writeln!(stdout, "^C")?;
+                    continue;
+                }
                 Err(e) => {
                     writeln!(stdout, "Error: {e:?}")?;
                     break;
