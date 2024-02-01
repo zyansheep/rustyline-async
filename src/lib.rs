9e265d0a037457785921be026ec50ded4a8939bd
Skyelar Craver <skyelar.craver@cellink.com>
2024-02-01 18:01:17 -0500

    add clear function to readline

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,7 @@ use std::{
 
 use crossterm::{
 	event::EventStream,
-	terminal::{self, disable_raw_mode},
+	terminal::{self, disable_raw_mode, Clear},
 	QueueableCommand,
 };
 use futures_channel::mpsc;
@@ -222,6 +222,12 @@ impl Readline {
 				buffer: Vec::new(),
 			},
 		))
+	}
+
+	pub fn clear(&mut self) -> Result<(), ReadlineError> {
+		self.raw_term.queue(Clear(terminal::ClearType::All))?;
+		self.line.clear_and_render(&mut self.raw_term)?;
+		Ok(())
 	}
 
 	/// Set maximum history length.  The default length is 1000.
