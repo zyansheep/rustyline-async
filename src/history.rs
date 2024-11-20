ace85def5addecc4a3c6bfb74e382bca108bcffc
Olaf Liebe <olaf.liebe@ollb.de>
2024-11-20 13:51:32 +0100

    Extended history API (get/set/clear/load/save).

--- src/history.rs
+++ src/history.rs
@@ -2,8 +2,8 @@ use std::collections::VecDeque;
 
 pub struct History {
 	// Note: old entries in front, new ones at the back.
-	pub entries: VecDeque<String>,
-	pub max_size: usize,
+	entries: VecDeque<String>,
+	max_size: usize,
 	current_position: Option<usize>,
 }
 impl Default for History {
@@ -32,6 +32,36 @@ impl History {
 			// Remove oldest entry
 			self.entries.pop_front();
 		}
+	}
+
+	// Changes the history size.
+	pub fn set_max_size(&mut self, max_size: usize) {
+		self.max_size = max_size;
+
+		while self.entries.len() > max_size {
+			// Remove oldest entry
+			self.entries.pop_front();
+		}
+
+		// Make sure we don't end up in an invalid position.
+		self.reset_position();
+	}
+
+	// Returns the current history entries.
+	pub fn get_entries(&self) -> &VecDeque<String> {
+		&self.entries
+	}
+
+	// Replaces the current history entries.
+	pub fn set_entries(&mut self, entries: impl IntoIterator<Item = String>) {
+		self.entries.clear();
+
+		// Using `add_entry` will respect `max_size` and remove duplicate lines etc.
+		for entry in entries.into_iter() {
+			self.add_entry(entry);
+		}
+
+		self.reset_position();
 	}
 
 	// Sets the history position back to the start.
@@ -70,8 +100,8 @@ impl History {
 }
 
 #[cfg(test)]
-#[tokio::test]
-async fn test_history() {
+#[test]
+fn test_history() {
 	let mut history = History::default();
 
 	history.add_entry("foo".into());
@@ -112,8 +142,8 @@ async fn test_history() {
 }
 
 #[cfg(test)]
-#[tokio::test]
-async fn test_history_limit() {
+#[test]
+fn test_history_limit() {
 	let mut history = History {
 		max_size: 3,
 		..Default::default()
@@ -128,4 +158,60 @@ async fn test_history_limit() {
 	assert_eq!(Some("baz"), history.search_next(""));
 	assert_eq!(Some("bar"), history.search_next(""));
 	assert_eq!(Some("bar"), history.search_next(""));
+
+	history.set_max_size(2);
+
+	assert_eq!(Some("qux"), history.search_next(""));
+	assert_eq!(Some("baz"), history.search_next(""));
+	assert_eq!(Some("baz"), history.search_next(""));
+}
+
+#[cfg(test)]
+#[test]
+fn test_history_reset_on_add() {
+	let mut history = History::default();
+
+	history.add_entry("foo".into());
+	history.add_entry("bar".into());
+	history.add_entry("baz".into());
+
+	assert_eq!(None, history.search_previous(""));
+	assert_eq!(Some("baz"), history.search_next(""));
+	assert_eq!(Some("bar"), history.search_next(""));
+
+	// This should reset the history position.
+	history.add_entry("qux".into());
+
+	assert_eq!(None, history.search_previous(""));
+	assert_eq!(Some("qux"), history.search_next(""));
+	assert_eq!(Some("baz"), history.search_next(""));
+	assert_eq!(Some("bar"), history.search_next(""));
+	assert_eq!(Some("foo"), history.search_next(""));
+}
+
+#[cfg(test)]
+#[test]
+fn test_history_export() {
+	let mut history = History {
+		max_size: 3,
+		..Default::default()
+	};
+
+	assert_eq!(history.get_entries(), &VecDeque::new());
+
+	history.add_entry("foo".into());
+	history.add_entry("bar".into());
+	history.add_entry("baz".into());
+
+	assert_eq!(history.get_entries(), &["foo", "bar", "baz"]);
+
+	history.add_entry("qux".into());
+
+	assert_eq!(history.get_entries(), &["bar", "baz", "qux"]);
+
+	history.set_entries(["a".to_string(), "b".to_string(), "b".to_string()]);
+
+	assert_eq!(Some("b"), history.search_next(""));
+	assert_eq!(Some("a"), history.search_next(""));
+	assert_eq!(Some("a"), history.search_next(""));
 }
